726263b673ae2d40fafdc423ccc5de97ba5a3b4b
Skripal9 <skripal9@gmail.com>
2022-09-22 22:51:45 +0200

    done task2

--- homeworckjs/task2.js
+++ homeworckjs/task2.js
@@ -1,12 +1,24 @@
 let welcom = prompt("Добро пожаловать в школу");
 let stringUp = "KAKOITO TEKST"
-
+let age = prompt("ваш реальный возраст?")
 
 if (welcom == '') {
     console.log("Строка пустая")
 } else {
     console.log("строка не пустая")
+    console.log(welcom.indexOf("лов"))
 }
 
 console.log(stringUp + ":" + welcom);
+
+
+if (age == 0) {
+    alert("вам рановато")
+} else if (age == 20) {
+    alert("вам ровно 20")
+} else {
+    alert("Не скажу сколько")
+}
+
+
 
