6c63c25a3886955c1c641540bc60ba24a3036910
Skripal9 <skripal9@gmail.com>
2022-09-22 11:00:49 +0200

    solve problem number

--- homeworckjs/task1.js
+++ homeworckjs/task1.js
@@ -1,7 +1,11 @@
-let number = prompt("ВВедите число")
-let value = true
-if (number == '') {
+let numb = prompt("ВВедите число");
+let value = true;
+if (numb == '') {
     alert(value);
-} else if (number !== '') {
-    alert(number);
+} else if (numb1 % 2 == 0) {
+    alert("anbeliwebel this number shere on 2");
+} else if (numb1 % 2) {
+    alert("Оно оказалось нечетное")
+} else if (numb1 !== '') {
+    alert('Поздравляем, число задано' + numb1);
 }
