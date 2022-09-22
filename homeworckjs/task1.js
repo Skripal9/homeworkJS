8d481f7ecacf5115b601462e72f7f14f2e5f9056
Skripal9 <skripal9@gmail.com>
2022-09-22 12:23:18 +0200

    seve

--- homeworckjs/task1.js
+++ homeworckjs/task1.js
@@ -1,4 +1,5 @@
 let numb = prompt("ВВедите число");
+let nechitnoe
 let value = true;
 if (numb == '') {
     alert(value);
@@ -8,8 +9,18 @@ if (numb == '') {
 
 if (numb % 2 == 0) {
     alert("anbeliwebel this number shere on 2");
-} else {
+} else if (numb % 2) {
+    let nechitnoe
     alert("Оно оказалось нечетное")
-} 
-
+    console.log("имя переменной - numb")
+    console.log(nechitnoe = Number(numb) + 3) 
+} else if (nechitnoe < numb){
+    console.log("имя переменной - numb")
+    console.log(Number(nechitnoe) - 10)    
+} else if (nechitnoe > numb){
+    console.log("Оно больше, чем нужно")
+  
+}
+/* console.log("имя переменной - numb")
 console.log(Number(numb) + 3) 
+ */
