3984bae32171c13255ccdeafdb670c760e656635
Skripal9 <skripal9@gmail.com>
2022-09-22 23:25:07 +0200

    done task3

---
+++ homeworckjs/task3.js
@@ -0,0 +1,12 @@
+/* Создайте объект, с данными: имя "Nick", возраст "55", роль "ceo".
+ */
+
+let userInfo = {
+    name: "Nick",
+    age: 55,
+    role: "ceo",
+    married: true,
+};
+
+userInfo.role = "Dnepr";
+console.log(userInfo);
