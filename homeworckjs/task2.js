5f38e3c9adb1682657ff3c3679a7cfdf660338ec
Skripal9 <skripal9@gmail.com>
2022-09-23 22:53:26 +0200

    homework excheng

--- homeworckjs/task2.js
+++ homeworckjs/task2.js
@@ -1,4 +1,35 @@
-let welcom = prompt("Добро пожаловать в школу");
+let welcomScool = 'Добро пожаловать в школу';
+
+if (welcomScool.length > 0) {
+    console.log('строка не пустая');
+} else {
+    console.log('Строка пустая');
+}
+
+let WelcomScooltoUpperCase = welcomScool.toUpperCase();
+console.log("WelcomScooltoUpperCase", WelcomScooltoUpperCase);
+
+let strignew = welcomScool + " : " + WelcomScooltoUpperCase;
+console.log("strignew", strignew);
+
+if (welcomScool.length > 0) {
+    var positionString = welcomScool.indexOf("лов");
+    console.log("positionString", positionString);
+} 
+
+
+
+/* 
+Если исходная строка не пустая, определить позицию подстроки "лов" и вывести в консоль в виде "Имя переменной - ее значение".
+Вывести модальное окно с текстом, полем для ввода текста и кнопками OK/Отмена.
+В котором будет текст "Ваш реальный возраст".
+Если "0" вывести в консоль "Вам рановато",
+если "20" вывести в консоль "Вам ровно 20" 
+другое - "Не скажу сколько".
+ */
+
+
+/* let welcom = prompt("Добро пожаловать в школу");
 let stringUp = "KAKOITO TEKST"
 let age = prompt("ваш реальный возраст?")
 
@@ -18,7 +49,6 @@ if (age == 0) {
     alert("вам ровно 20")
 } else {
     alert("Не скажу сколько")
-}
-
+} */
 
 
