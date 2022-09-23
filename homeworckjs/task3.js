ba83f24786c7a9965e360c9f5bb93e98cbeeba24
Skripal9 <skripal9@gmail.com>
2022-09-23 23:09:08 +0200

    homework excheng task 2

--- homeworckjs/task3.js
+++ homeworckjs/task3.js
@@ -1,12 +1,19 @@
-/* Создайте объект, с данными: имя "Nick", возраст "55", роль "ceo".
- */
 
 let userInfo = {
     name: "Nick",
     age: 55,
     role: "ceo",
-    married: true,
 };
+userInfo.married = true;
 
-userInfo.role = "Dnepr";
-console.log(userInfo);
+var fillCoud = 0
+for (key in userInfo) {
+    fillCoud++;
+    console.log(key + " - " + userInfo[key]);
+ }
+console.log("fillCoud", fillCoud);
+
+if ("role" in userInfo) {
+    userInfo["City"] = "Dnepr"
+    console.log(userInfo);
+}
